fdd45f67a5f593dd9b0c035bd9336c00bf0eb308
Mike Andrade <mike.andrade.oliveira@gmail.com>
2022-11-30 19:15:19 -0300

    Creating Customer Model

--- src/main/java/com/mike/controller/MikeController.java
+++ src/main/java/com/mike/controller/MikeController.java
@@ -3,11 +3,19 @@ package com.mike.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class MikeController {
 
-    @GetMapping("/")
-    public String greet(){
-        return "Hello!";
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        GreetResponse response = new GreetResponse("Hello",
+                List.of("Java", "Ruby", "Python"),
+                new Person("Alex", 28, 30000));
+        return response;
     }
+
 }
+record Person(String name, int age, double savings){}
+record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person){}
