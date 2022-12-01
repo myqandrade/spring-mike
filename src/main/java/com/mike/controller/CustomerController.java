178c48f1a8d19d1ef8338d5a7d74ec0e6a0c6854
Mike Andrade <mike.andrade.oliveira@gmail.com>
2022-12-01 20:35:27 -0300

    Inserting data in db

---
+++ src/main/java/com/mike/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.mike.controller;
+
+import com.mike.model.Customer;
+import com.mike.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomerController {
+
+    @Autowired
+    private final CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+}
--- src/main/java/com/mike/model/Customer.java
+++ src/main/java/com/mike/model/Customer.java
@@ -6,8 +6,7 @@ import java.util.Objects;
 @Entity
 public class Customer {
     @Id
-    @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
